2b8fbf00dfbba0d6f5f44d9d42fec51422e3ea1f
Victoria Burdo <inorit@list.ru>
2023-08-01 23:32:39 +0200

    Add fully completed 1st task

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,7 @@
+import os
+import time
 import unittest
+import allure
 from optparse import OptionParser
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
@@ -12,10 +15,21 @@ capabilities = dict(
     appActivity='com.android.calculator2.Calculator'
 )
 
+browser_capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android Emulator',
+    appPackage='com.google.android.googlequicksearchbox',
+    appActivity='com.google.android.apps.gsa.queryentry.QueryEntryActivity'
+)
+
 appium_server_url = 'http://localhost:4723'
 
 
+@allure.feature('Calculator test')
 class TestAppium(unittest.TestCase):
+    results = None
+
     def setUp(self) -> None:
         self.driver = webdriver.Remote(appium_server_url, capabilities)
 
@@ -24,7 +38,31 @@ class TestAppium(unittest.TestCase):
         if self.driver:
             self.driver.quit()
 
+        TestAppium.results = self._outcome.result
 
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        if not hasattr(cls.results, 'testsRun'):
+            return
+
+        driver = webdriver.Remote(appium_server_url, browser_capabilities)
+
+        result_string = f'passed: {cls.results.testsRun - len(cls.results.errors) - len(cls.results.failures)}, failed: {len(cls.results.errors) + len(cls.results.failures)}, total: {cls.results.testsRun}'
+
+        if driver:
+            time.sleep(5)
+            driver.find_element(value='com.google.android.googlequicksearchbox:id/search_box').send_keys(result_string)
+
+            time.sleep(5)
+
+            with open('screenshot.png', 'wb') as file:
+                file.write(driver.get_screenshot_as_png())
+
+            driver.quit()
+
+
+    @allure.title('Check the correctness of adding')
     def test_four_add_eight(self) -> None:
         self.driver.find_element(value='com.google.android.calculator:id/digit_4').click()
         self.driver.find_element(value='com.google.android.calculator:id/op_add').click()
@@ -37,6 +75,7 @@ class TestAppium(unittest.TestCase):
         assert(self.driver.find_element(value='com.google.android.calculator:id/result_final').text == '12')
 
 
+    @allure.title('Check the correctness of dividing')
     def test_nine_div_three(self) -> None:
         self.driver.find_element(value='com.google.android.calculator:id/digit_9').click()
         self.driver.find_element(value='com.google.android.calculator:id/op_div').click()
@@ -49,26 +88,39 @@ class TestAppium(unittest.TestCase):
         assert(self.driver.find_element(value='com.google.android.calculator:id/result_final').text == '3')
 
 
+    @allure.title('Check handling of zero division')
     def test_zero_div(self) -> None:
-        self.driver.find_element(value='com.google.android.calculator:id/digit_9').click()
-        self.driver.find_element(value='com.google.android.calculator:id/op_div').click()
-        self.driver.find_element(value='com.google.android.calculator:id/digit_0').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/digit_9').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/op_div').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/digit_0').click()
+
+        os.system('adb shell input tap 600 1400')
+        os.system('adb shell input tap 900 1100')
+        os.system('adb shell input tap 200 2200')
 
         assert(self.driver.find_element(value='com.google.android.calculator:id/result_preview').text == '')
 
-        self.driver.find_element(value='com.google.android.calculator:id/eq').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/eq').click()
+        os.system('adb shell input tap 900 2200')
 
         assert(self.driver.find_element(value='com.google.android.calculator:id/result_preview').text == 'Can\'t divide by 0')
 
 
+    @allure.title('Check handling of the root of a negative number')
     def test_root_from_negative(self) -> None:
-        self.driver.find_element(value='com.google.android.calculator:id/op_sqrt').click()
-        self.driver.find_element(value='com.google.android.calculator:id/op_sub').click()
-        self.driver.find_element(value='com.google.android.calculator:id/digit_5').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/op_sqrt').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/op_sub').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/digit_5').click()
+
+        os.system('adb shell input tap 100 900')
+        os.system('adb shell input tap 900 1700')
+        os.system('adb shell input tap 400 1700')
 
         assert(self.driver.find_element(value='com.google.android.calculator:id/result_preview').text == '')
 
-        self.driver.find_element(value='com.google.android.calculator:id/eq').click()
+        # self.driver.find_element(value='com.google.android.calculator:id/eq').click()
+
+        os.system('adb shell input tap 900 2200')
 
         assert(self.driver.find_element(value='com.google.android.calculator:id/result_preview').text == 'Keep it real')
 
@@ -76,8 +128,8 @@ class TestAppium(unittest.TestCase):
 def get_options():
     parser = OptionParser()
 
-    parser.add_option("-d", "--device_name", dest="device_name", default=None)
-    parser.add_option("-t", "--test_case", dest="test_cases", action='append', default=[])
+    parser.add_option('-d', '--device_name', dest='device_name', default=None)
+    parser.add_option('-t', '--test_case', dest='test_cases', action='append', default=[])
 
     (options, _) = parser.parse_args()
 
@@ -95,7 +147,7 @@ def form_suite(options):
     ]
 
     if options.device_name:
-        capabilities["udid"] = options.device_name
+        capabilities['udid'] = options.device_name
     if not len(options.test_cases):
         suite.addTests(test_cases)
         return suite
